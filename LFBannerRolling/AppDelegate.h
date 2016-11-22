44686c0a87c996e066879a077d00e4460b2844f4
empire-lfang <745803855@qq.com>
2016-11-22 15:34:05 +0800

    Initial Commit

---
+++ LFBannerRolling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFBannerRolling
+//
+//  Created by daoxiu on 2016/11/22.
+//  Copyright © 2016年 aowokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFBannerRolling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFBannerRolling
+//
+//  Created by daoxiu on 2016/11/22.
+//  Copyright © 2016年 aowokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
